ef7897da6a2e0595ab1211b903a3e0e840d4925e
Deyan Ginev <d.ginev@jacobs-university.de>
2018-09-30 17:06:55 -0400

    update dataset check for 08.2018 data

--- src/dataset_check.py
+++ src/dataset_check.py
@@ -10,9 +10,10 @@ import numpy as np
 import json
 import warnings
 import gc
+import tarfile
 
 # We can not load the npz file, as we don't fit the new hash in RAM...
-root_para_dir = "/var/local/ams-paragraphs"
+ams_para_model = "/var/local/ams_paragraphs_arxmliv_08_2018.tar"
 
 labels = sorted(["acknowledgement", "algorithm", "assumption", "caption", "case", "condition", "conjecture", "corollary", "definition", "example",
                  "fact", "lemma", "notation", "other", "paragraph", "problem", "proof", "proposition", "question", "remark", "result", "step", "theorem"])
@@ -32,31 +33,43 @@ total_duplicates = 0
 distinct_paragraphs = 0
 
 paragraph_size = {}
+label_para_size = {}
+for label in labels:
+    label_para_size[label] = {}
 
 # Do the regular traversal done for the .npz map, but instead of creating x,y lists for training,
 # compute statistics immediately.
-for label_idx, label in enumerate(labels):
-    print("Processing dir: ", label)
-    label_dir = os.path.join(root_para_dir, label)
-    for para_file in os.listdir(label_dir):
-        if para_file.endswith(".txt"):
-            w_val = []
-            words = open(os.path.join(label_dir, para_file),
-                         "r").read().split()
-            total_paragraphs += 1
-            len_key = len(words)
 
-            for word in words:
-                if word in w_index:
-                    w_val.append(w_index[word])
+tar = tarfile.open(ams_para_model, "r")
+while True:
+    tarinfo = tar.next()
+    if tarinfo is None:
+        break
+    total_paragraphs += 1
+    label = tarinfo.name.split('/')[0]
 
-            paragraph_size[len_key] = 1 + paragraph_size.get(len_key, 0)
-            x = ','.join(str(item) for item in w_val)
-            xs_hash.setdefault(x, []).append(label)
+    words = tar.extractfile(tarinfo).read().decode('utf-8').split()
+
+    w_val = []
+    for word in words:
+        if word in w_index:
+            w_val.append(w_index[word])
+    len_key = len(w_val)
+
+    paragraph_size[len_key] = 1 + paragraph_size.get(len_key, 0)
+    label_para_size[label][len_key] = 1 + \
+        label_para_size[label].get(len_key, 0)
+
+    x = ','.join(str(item) for item in w_val)
+    xs_hash.setdefault(x, []).append(label)
+    if total_paragraphs % 10_000 == 0:
+        print("Processed %d paragraphs." % total_paragraphs)
 
 ps_json = json.dumps(paragraph_size)
 open("data/ams_paragraph_sizes.json", "w").write(ps_json)
 
+ls_json = json.dumps(label_para_size)
+open("data/ams_label_paragraph_sizes.json", "w").write(ls_json)
 
 iteration = 0
 for xs_key, val_list in xs_hash.items():
@@ -79,6 +92,8 @@ for xs_key, val_list in xs_hash.items():
     if iteration % 1_000_000 == 0:
         gc.collect()
 
+print("Total paragraphs analyzed: ", total_paragraphs)
+
 unique_single_total = 0
 print("Paragraphs that appear once in distinct single classes:")
 for key, val in unique_singles.items():
@@ -91,7 +106,6 @@ for key, val in duplicate_singles.items():
     dup_single_total += val
     print("class ", key, " :", val)
 
-print("Total paragraphs analyzed: ", total_paragraphs)
 print("Distinct paragraphs analyzed: %d (%.2f percent of total)" %
       (distinct_paragraphs, distinct_paragraphs/total_paragraphs*100))
 
@@ -101,3 +115,64 @@ print("Distinct paragraphs, appearing more than once, only in the same class: %d
       (dup_single_total, (dup_single_total/distinct_paragraphs*100)))
 print("Distinct paragraphs, appearing in 2+ classes: %d (%.2f percent of distinct)" %
       (total_duplicates, (total_duplicates/distinct_paragraphs*100)))
+
+# arXMLiv 08.2018 results
+#
+# Total paragraphs analyzed:  35885000
+#
+# Paragraphs that appear once in distinct single classes:
+#
+# class  other  : 22169457
+# class  definition  : 795301
+# class  lemma  : 1420394
+# class  corollary  : 465518
+# class  theorem  : 1452875
+# class  proof  : 5310115
+# class  proposition  : 1023122
+# class  remark  : 800879
+# class  notation  : 60597
+# class  example  : 409038
+# class  conjecture  : 47282
+# class  algorithm  : 12176
+# class  acknowledgement  : 4857
+# class  fact  : 20566
+# class  problem  : 33175
+# class  assumption  : 30065
+# class  result  : 3072
+# class  step  : 12676
+# class  question  : 8577
+# class  condition  : 4159
+# class  caption  : 1002
+# class  case  : 4211
+# class  paragraph  : 4816
+#
+# Repeats in single classes:
+#
+# class  other  : 433783
+# class  lemma  : 21593
+# class  theorem  : 29770
+# class  remark  : 5804
+# class  notation  : 1151
+# class  proposition  : 15122
+# class  proof  : 45053
+# class  corollary  : 5673
+# class  definition  : 20169
+# class  example  : 4557
+# class  conjecture  : 940
+# class  fact  : 483
+# class  acknowledgement  : 115
+# class  assumption  : 622
+# class  result  : 45
+# class  problem  : 398
+# class  case  : 245
+# class  algorithm  : 450
+# class  question  : 87
+# class  step  : 207
+# class  condition  : 96
+# class  paragraph  : 53
+# class  caption  : 121
+#
+# Distinct paragraphs analyzed: 34712798 (96.73 percent of total)
+# Distinct paragraphs appearing once in corpus: 34093930 (98.22 percent of distinct)
+# Distinct paragraphs, appearing more than once, only in the same class: 586537 (1.69 percent of distinct)
+# Distinct paragraphs, appearing in 2+ classes: 32331 (0.09 percent of distinct)
