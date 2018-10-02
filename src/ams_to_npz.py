5515269f425bfe7d20a653d6d32a26b73dfbba0d
Deyan Ginev <d.ginev@jacobs-university.de>
2018-10-02 14:50:41 -0400

    now can work with 33.5 million paragraphs, thanks to hdf5

--- src/ams_to_npz.py
+++ src/ams_to_npz.py
@@ -15,23 +15,13 @@ import zipfile
 import io
 import sys
 import tarfile
-
-
-def saveCompressed(fh, **namedict):
-    with zipfile.ZipFile(fh, mode="w", compression=zipfile.ZIP_DEFLATED,
-                         allowZip64=True) as zf:
-        for k, v in namedict.items():
-            with zf.open(k + '.npy', 'w', force_zip64=True) as buf:
-                np.lib.npyio.format.write_array(buf,
-                                                np.asanyarray(v),
-                                                allow_pickle=True)
-
+import h5py
 
 # Defaults
 vocab_file = "data/vocab.txt"
 ams_para_model = "/var/local/ams_paragraphs_arxmliv_08_2018.tar"
-destination = "/var/local/full_ams_08_2018.npz"
-
+destination = "/var/local/full_ams_08_2018"
+max_words = 480
 
 argcount = len(sys.argv[1:])
 if argcount > 0:
@@ -40,6 +30,8 @@ if argcount > 0:
         ams_para_model = sys.argv[2]
         if argcount > 2:
             destination = sys.argv[3]
+            if argcount > 3:
+                max_words = int(sys.argv[4])
 
 labels = sorted(["acknowledgement", "algorithm", "assumption", "caption", "case", "condition", "conjecture", "corollary", "definition", "example",
                  "fact", "lemma", "notation", "other", "paragraph", "problem", "proof", "proposition", "question", "remark", "result", "step", "theorem"])
@@ -61,6 +53,18 @@ for label_idx, label in enumerate(labels):
 para_idx = 0
 
 tar = tarfile.open(ams_para_model, "r")
+# Big chunks for quick continuous access, but still be able to fit in RAM
+chunk_count = 1_000_000  # tune this by hand for smaller data.
+
+fp = h5py.File(destination+".hdf5", "w")
+x_dset = fp.create_dataset("x", (chunk_count, max_words), maxshape=(
+    None, max_words), chunks=(chunk_count, max_words))
+print("x_dset chunks: ", x_dset.chunks)
+
+y_dset = fp.create_dataset("y", (chunk_count,), maxshape=(
+    None,), chunks=(chunk_count,))
+print("y_dset chunks: ", y_dset.chunks)
+
 while True:
     tarinfo = tar.next()
     if tarinfo is None:
@@ -75,23 +79,37 @@ while True:
     words = tar.extractfile(tarinfo).read().decode('utf-8').split()
     for word in words:
         if word in w_index:
-            w_val.append(w_index[word])
+            w_val.extend([w_index[word]])
         # else:
             # Should we drop or use a fake number? Drop for now
             # w_val.append(-1)
             # print("unk: ", word)
+    w_val = np.array(w_val[:max_words])
+    npad = max_words-len(w_val)
+    if npad > 0:
+        w_val = np.pad(w_val, (0, npad), mode='constant')
     x_paras.append(w_val)
     y_labels.append(label_idx)
-    if para_idx % 10000 == 0:  # reset members every 100 files, to deallocate memory
+    if para_idx % 10_000 == 0:  # reset members every 100 files, to deallocate memory
         tar.members = []
-        print("at paragraph %d : ", para_idx)
+        print("at paragraph %d : " % para_idx)
         for label in labels:
             print("-- found %d of %s" % (label_para_count[label], label))
         print("---")
+        if para_idx % chunk_count == 0:
+            x_dset.resize(x_dset.shape[0]+chunk_count, axis=0)
+            x_dset[-chunk_count:] = x_paras[:]
+            x_paras = []
+            print(x_dset.shape)
+            y_dset.resize(y_dset.shape[0]+chunk_count, axis=0)
+            y_dset[-chunk_count:] = y_labels[:]
+            y_labels = []
+            print(y_dset.shape)
 
 tar.close()
 
 for label in labels:
     print("found %d of %s" % (label_para_count[label], label))
 
-saveCompressed(destination, x=x_paras, y=y_labels)
+print("---")
+print("total collected paragraphs: ", para_idx)
